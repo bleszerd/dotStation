a7eba7e6856559383fb2a66945999e4f3d004bd7
Vinicius Resende <alive2k@programmer.net>
2021-02-20 14:37:57 -0300

    force redeploy

--- src/components/HomePage/Projects/index.tsx
+++ src/components/HomePage/Projects/index.tsx
@@ -7,13 +7,13 @@ const projectsData = [
         title: "Projeto #001",
         image: "https://www.crushpixel.com/big-static14/preview4/test-tube-flask-with-blue-1672814.jpg",
         description: "Aqui ficaria uma descrição do projeto vinculado a uma postagem no blog.",
-        blogURL: "/blog/Publicação_de_teste_-_%23001@602ff9255381042a551c1f1e"
+        blogURL: "/blog/Publica%C3%A7%C3%A3o_de_deste_-_%23001@603147a13f50551c5dec3603"
     },
     {
         title: "Projeto #002",
         image: "https://image.shutterstock.com/z/stock-photo-test-tubes-and-erlenmeyer-flask-in-laboratory-228473260.jpg",
         description: "Aqui ficaria uma descrição do projeto vinculado a uma postagem no blog.",
-        blogURL: "/blog/Publicação_de_teste_-__%23002@602ff9285381042a551c1f1f"
+        blogURL: "/blog/Publicação_de_deste_-_%23002@603147a63f50551c5dec3604"
     }
 ]
 
